38f6db99905e522f033480abac2988711a63fd07
Harsh Sinha <harshputu@gmail.com>
2024-03-23 02:50:34 +0530

    Js5 completed

---
+++ Javascript/JS5.js
@@ -0,0 +1,46 @@
+  const student={
+    name:"harsh",
+    age:23,
+    marks:94.4
+  };
+
+  const item={
+    price:99,
+    discount:10,
+    color:["blue","white"]
+  };
+
+  const tweet={
+    username:"@harshputu",
+    content:"Hey Guys I have started my journey to become succesful slowly and gradually",
+    likes:678890,
+    reposts:10,
+    tags:["#@chotamerabhai","@raiderX"]
+  };
+
+  //object of object
+  const clasInfo={
+    harsh:{
+      grade:"A+",
+      city:"Delhi"
+    },
+    chota:{
+      grade:"O",
+      city:"Chandigarh"
+    }
+  };
+
+  //Array of object
+  
+  const arrInfo=[
+    {
+      name:"harsh",
+      grade:"A+",
+      city:"Delhi"
+    },
+    {
+      name:"chota",
+      grade:"O",
+      city:"Mumbai"
+    }
+  ]
