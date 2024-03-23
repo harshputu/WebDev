53c40093fcad3abcde4f484cfda6b18be5fdd82a
Harsh Sinha <harshputu@gmail.com>
2024-03-23 14:58:43 +0530

    Practice5

---
+++ Javascript/practice5.js
@@ -0,0 +1,20 @@
+//Question 1: random number between 1 to 6
+console.log(Math.floor(Math.random()*6)+1);
+
+//Create an object represent ing a car that stores the following properties for the car:name,model,color.
+const car={
+    name:"Hyundai",
+    model:"Creta",
+    color:"Abyss Black"
+};
+console.log(car.name);
+
+//Create an object with the name agen and city. Edit theior city original value change it to "New York"
+
+const person={
+    name:"Harsh",
+    age:"23",
+    city:"Pune"
+};
+person.city="New York";
+person.country="United states";
